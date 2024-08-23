01e3ab2da8664706dd3c57eac5833898abded224
tk-violin <taiki_kubo@berkeley.edu>
2024-08-23 11:22:52 -0700

    trialcomment by tk-violin

--- newton.py
+++ newton.py
@@ -1,5 +1,5 @@
 import warnings
-import numpy as np # tk-violin
+import numpy as np # comment by tk-violin
 
 def first_derivative(f, x, eps = 0.01):
     """ Find first derivative of given function evaluated at a given point
